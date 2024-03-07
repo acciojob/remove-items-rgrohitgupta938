cdcc71cd70a3b2076ad475b63f46a37e06789dfa
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-07 10:49:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function removeColor() {
+            var selectElement = document.getElementById("colorSelect");
+            var selectedOption = selectElement.options[selectElement.selectedIndex];
+            selectElement.removeChild(selectedOption);
+        }
